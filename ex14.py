9c82b31464ce63b8c589669fc334c4eeb7f68c01
boelterr <bielboelter@gmail.com>
2021-08-26 16:07:07 -0300

    Create ex14.py

---
+++ ex14.py
@@ -0,0 +1,8 @@
+valor = int(input('Digite um número para saber a tabuada: '))  
+x = 0  
+print("*" * 18)  
+print("Tabuada de {}".format(valor))  
+print("*" * 18)  
+while(x <= 10):  
+  print("{0} X {1} = {2}"8.format(x, valor, (x * valor)))  
+  x = x + 1
